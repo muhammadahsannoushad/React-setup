a76f26584f165a45f1c526ff0a73403769ecafe6
Muhammad Ahsan <muhammadahsannoushad@gmail.com>
2024-06-29 20:18:15 +0500

    import navbar

--- src/App.js
+++ src/App.js
@@ -2,28 +2,17 @@
 import './App.css';
 import About from './component/About';
 import Home from './component/Home';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Ahsan
-        </a>
-      </header> */}
+        <Navbar/>
        <h2>Learn React with Ahsan</h2>
        <h3>What is is your name </h3>
        <Home/>
        <About/>
+     
     </div>
   );
 }  
